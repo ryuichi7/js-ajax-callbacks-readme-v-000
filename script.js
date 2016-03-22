839fc968150976de07c85a0e032a1ef5b8df0964
Brett <brettrheenan@gmail.com>
2016-03-22 12:52:40 +0100

    Done.

--- script.js
+++ script.js
@@ -7,4 +7,12 @@ $(document).ready(function(){
     // insert the response
     $("#sentences").html(response);
   });
+
+  var url = "https://api.github.com/repos/rails/rails/commits?sha=82885325e04d78fb7ec608a4670164d842d23078";
+ 
+$.get(url)
+  .done(function(data) {
+    console.log("Done");
+    console.log(data);
+  });
 });
